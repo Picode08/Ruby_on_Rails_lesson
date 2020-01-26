a4212e57f457f4b0cc25a49a82c4b5b2a48886fc
Chikako <chikasienne@gmail.com>
2020-01-26 14:53:57 +0000

    9章9.5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
 	validates :content, presence: true, length: { maximum: 100 }
 	validates :content, presence: true
+	
+	def user
+		User.find(self.user_id)
+	end
 end
